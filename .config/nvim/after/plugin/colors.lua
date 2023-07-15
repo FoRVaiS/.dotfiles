3cba2e00bd958171d0c95c085c1a5a17b779ed44
Benjamin Chiang <chiangbenjamin991@gmail.com>
2023-07-15 02:38:23 -0700

    feat(nvim): enable highlighting for diff adds and deletes

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -3,6 +3,16 @@ function ColorTheme()
 
   vim.g.codedark_transparent = 1
   vim.cmd.colorscheme(color)
+
+  local highlights = {
+    'hi link @text.diff.add DiffAdd',
+    'hi link @text.diff.delete DiffDelete',
+    -- Add any other commands you need here
+  }
+
+  for _, highlight in ipairs(highlights) do
+      vim.cmd(highlight)
+  end
 end
 
 ColorTheme()
