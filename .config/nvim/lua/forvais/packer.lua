74b81b736c9595aad6d890fed23ff0cff15ad2d8
Benjamin Chiang <chiangbenjamin991@gmail.com>
2023-07-24 00:17:32 -0700

    style(nvim): format packer.lua

--- .config/nvim/lua/forvais/packer.lua
+++ .config/nvim/lua/forvais/packer.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -18,14 +18,14 @@ vim.cmd([[
   augroup end
 ]])
 
-local status, packer = pcall(require, "packer")
+local status = pcall(require, "packer")
 if not status then
-	return
+  return
 end
 
-return require('packer').startup(function (use)
+return require('packer').startup(function(use)
   -- Plugin management
-  use 'wbthomason/packer.nvim'
+  use('wbthomason/packer.nvim')
 
   -- Appearance
   use({
@@ -37,7 +37,7 @@ return require('packer').startup(function (use)
   use({
     'nvim-telescope/telescope.nvim',
     requires = {
-      {'nvim-lua/plenary.nvim'}
+      { 'nvim-lua/plenary.nvim' }
     }
   })
   use('christoomey/vim-tmux-navigator')
@@ -52,7 +52,7 @@ return require('packer').startup(function (use)
   use('lewis6991/gitsigns.nvim')
 
   -- Language Support and Editing
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('nvim-treesitter/nvim-treesitter-refactor')
   use('joosepalviste/nvim-ts-context-commentstring')
   use('tpope/vim-commentary')
@@ -60,23 +60,23 @@ return require('packer').startup(function (use)
     'VonHeikemen/lsp-zero.nvim',
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
 
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
     }
   })
   use('jiangmiao/auto-pairs')
