bd1ad1845b3f802e1a3be42d0f0a9b303caf8713
Benjamin Chiang <chiangbenjamin991@gmail.com>
2023-08-20 14:07:11 -0700

    refactor(nvim): change the color of diff text

--- .config/nvim/lua/forvais/plugins/colors.lua
+++ .config/nvim/lua/forvais/plugins/colors.lua
@@ -7,6 +7,7 @@ function ColorTheme()
   local highlights = {
     'hi link @text.diff.add DiffAdd',
     'hi link @text.diff.delete DiffDelete',
+    'highlight DiffText ctermfg=15 ctermbg=64',
     -- Add any other commands you need here
   }
 
