acbc50ee474a22e090d2c3d0713a913db4c4095c
Benjamin Chiang <chiangbenjamin991@gmail.com>
2023-09-04 11:26:04 -0700

    feat(nvim): configure dap to debug javascript

--- .config/nvim/lua/forvais/plugins/dap.lua
+++ .config/nvim/lua/forvais/plugins/dap.lua
@@ -2,3 +2,28 @@ local dap = require("dap")
 
 vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>dr", ":DapContinue<CR>", { noremap = true })
+
+dap.adapters["pwa-node"] = {
+  type = "server",
+  host = "127.0.0.1",
+  port = 8123,
+  executable = {
+    command = "js-debug-adapter",
+  }
+}
+
+for _, language in ipairs { "typescript", "javascript" } do
+  dap.configurations[language] = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      cwd = vim.fn.getcwd(),
+      sourceMaps = true,
+      protocol = "inspector",
+      console = "integratedTerminal",
+      runtimeExecutable = vim.fn.exepath("node"),
+    },
+  }
+end
