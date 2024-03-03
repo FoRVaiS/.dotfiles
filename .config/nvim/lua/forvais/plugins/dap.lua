0758b0aceb44c4cdf26cba525b3fc372cad64596
Benjamin Chiang <chiangbenjamin991@gmail.com>
2024-03-03 12:09:05 -0800

    feat(nvim): configure dap to debug python

--- .config/nvim/lua/forvais/plugins/dap.lua
+++ .config/nvim/lua/forvais/plugins/dap.lua
@@ -28,36 +28,66 @@ for _, language in ipairs { "typescript", "javascript" } do
   }
 end
 
-dap.adapters.python = {
-  type = 'executable',
-  command = 'python3',
-  args = {'-m', 'debugpy.adapter'},
-}
+dap.adapters.python = function(cb, config)
+  if config.request == 'attach' then
+    ---@diagnostic disable-next-line: undefined-field
+    local port = (config.connect or config).port
+    ---@diagnostic disable-next-line: undefined-field
+    local host = (config.connect or config).host or '127.0.0.1'
+    cb({
+      type = 'server',
+      port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+      host = host,
+      options = {
+        source_filetype = 'python',
+      },
+    })
+  else
+    cb({
+      type = 'executable',
+      command = '/usr/bin/python3',
+      args = { '-m', 'debugpy.adapter' },
+      options = {
+        source_filetype = 'python',
+      },
+    })
+  end
+end
 
 dap.configurations.python = {
   {
     type = 'python',
     request = 'launch',
-    name = "Debug Current File",
+    name = 'Debug Current File',
     program = "${file}",
-    pythonPath = "/path/to/your/venv/bin/python3",
-    args = function()
-      local arguments = vim.fn.input("Arguments: ")
-      return vim.split(arguments, " ")
+    args = function ()
+      local args = vim.fn.input('Arguments: ', '')
+      args = vim.split(args, ' ')
+      return args
     end
   },
   {
     type = 'python',
     request = 'launch',
-    name = "Debug Current File in venv",
+    name = 'Debug Current File (venv)',
     program = "${file}",
     pythonPath = function()
-      local venv_path = vim.fn.input("Path to venv: ")
-      return venv_path.."/bin/python3"
+      local venv_path = vim.fn.input('Path to venv : ', './venv')
+
+      if venv_path == '' then
+        venv_path = './venv'
+      end
+
+      if venv_path:sub(-1) ~= '/' then
+        venv_path = venv_path .. '/'
+      end
+
+      return venv_path .. 'bin/python'
     end,
-    args = function()
-      local arguments = vim.fn.input("Arguments: ")
-      return vim.split(arguments, " ")
+    args = function ()
+      local args = vim.fn.input('Arguments: ', '')
+      args = vim.split(args, ' ')
+      return args
     end
   }
 }
